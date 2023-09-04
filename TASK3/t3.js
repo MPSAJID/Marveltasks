54d50d510eca47f6ba7095c5d62d74c4505af254
MPSAJID <122434439+MPSAJID@users.noreply.github.com>
2023-09-04 16:10:38 +0530

    task update

---
+++ TASK3/t3.js
@@ -0,0 +1,24 @@
+const New = document.querySelector('#rec');
+New.addEventListener('click',Cook);
+function recipe(stepNumber,stepDescription,callback){
+    return new Promise((resolve , reject)=>{
+        const stepElement = document.querySelector(`#p${stepNumber}`);
+        stepElement.innerHTML= `${stepNumber}: ${stepDescription}`;
+        setTimeout(()=>{
+            callback;
+            resolve();
+        }, 1000);
+    })
+}
+function Cook(){
+    recipe(1, "Wash the mutton and marinate it with spices.")
+    .then(()=>recipe(2, "Soak the rice in water for 30 minutes."))
+    .then(()=> recipe(3, "Fry the onions until golden brown."))
+    .then(()=>recipe(4, "Cook the marinated mutton until tender."))
+    .then(()=>recipe(5, "Boil the soaked rice until it's 70% cooked."))
+    .then(()=>recipe(6, "Layer the rice and mutton in a large pot."))
+    .then(()=> recipe(7, "Add saffron milk and ghee on top."))
+    .then(()=>recipe(8, "Cover and cook on low heat until done."))
+    .then(()=>recipe(9, "Let it rest for 10 minutes before serving."))
+    .then(()=>recipe(10, "Serve your delicious Hyderabadi Mutton Biryani!"))
+}
